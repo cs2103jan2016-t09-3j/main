a0c90fa9c1d13f2c79a577e03188f250212d2107
sanfire <a0124131@u.nus.edu>
2016-02-15 18:20:40 +0800

    UI skeleton added

--- src/TNoteMain.java
+++ src/TNoteMain.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class TNoteMain {
-
+	
+	private Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Logic logic = new Logic();
+		System.out.println(logic.welcomeMessage);
+		
+		while(true){
+		String userInput = sc.nextLine();
+		
+		String response = logic.executeCommand(userInput);
+		
+		System.out.println(response);
+		}
 
 	}
 
