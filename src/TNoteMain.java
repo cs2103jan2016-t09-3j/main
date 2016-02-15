dc2d8053104c4a2eea8cea577c834814265508d4
sanfire <a0124131@u.nus.edu>
2016-02-15 18:02:16 +0800

    New project Start

---
+++ src/TNoteMain.java
@@ -0,0 +1,9 @@
+
+public class TNoteMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/TestCommit.java
+++
@@ -1,4 +0,0 @@
-
-public class TestCommit {
-// hi joelle here
-}
