5d11625833a77b753ef1bc8c63c729145f0fbc7a
l by <luo.beiyu@yahoo.com>
2016-02-15 19:16:18 +0800

    checkAdd first try

--- src/TNotesParser.java
+++ src/TNotesParser.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
 public class TNotesParser {
-
+	private ArrayList<String> list = new ArrayList<String>();
+	public ArrayList<String> checkAdd(String inputString){
+		String errorMessage = "invalid commands";
+		String arr[] = inputString.split(" ", 2);
+		String firstWord = arr[0].toLowerCase();
+		list.add(firstWord);
+		String theRest = arr[1].toLowerCase();
+		list.add(theRest);
+		
+		try{ 
+			if( firstWord.equals(" ")){
+				return list;
+			}
+		}
+			catch (Exception e){
+				list.add(errorMessage);
+				return list;
+			}
+		return list;
+		}
 }
+	
+
+
