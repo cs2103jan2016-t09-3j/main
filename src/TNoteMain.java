d99f15553164ae35f87ca0987da5b04a43127473
sanfire <a0124131@u.nus.edu>
2016-02-15 20:23:20 +0800

    change to test logic

--- src/TNoteMain.java
+++ src/TNoteMain.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 public class TNoteMain {
 	
-	private Scanner sc = new Scanner(System.in);
+	private static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Logic logic = new Logic();
-		System.out.println(logic.welcomeMessage);
+		System.out.println(logic.welcomeMessage());
 		
 		while(true){
 		String userInput = sc.nextLine();
 		
-		String response = logic.executeCommand(userInput);
+		logic.executeCommand(userInput);
 		
-		System.out.println(response);
+		//System.out.println(response);
 		}
-//nnn
+
 	}
 
 }
