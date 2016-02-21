bddd4f59fac133cb5bdf70064731fb312dcd768b
Joelle Lim <joellelim94@gmail.com>
2016-02-21 16:00:29 +0800

    made newly added task file class

--- src/TNotesStorage.java
+++ src/TNotesStorage.java
@@ -3,93 +3,97 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-
-public class TNotesStorage {
-	String eventName;
-	String date; 
-	String time;
-	
-	
-	public TNotesStorage(ArrayList<String> list){
-		eventName=list.get(0);
-		date=list.get(1);
-		time=list.get(2);
+// Note that must change storage to NotesStorage when using commit
+public class TNotesStorage {		
 		
-	}
-	// TESting
-	public static void main(String[] args) {
-		ArrayList<String> inputList = new ArrayList<String>();
-		inputList.add("call dad");
-		inputList.add("yesterday");
-		inputList.add("12pm");
-		
-		TNotesStorage test = new TNotesStorage(inputList);
-		if(test.addThisEvent()){
-			System.out.println("this works");
+		public TNotesStorage (ArrayList<String> list) {
+			taskFile newTask = new taskFile(list);	   
 		}
-	}
-	
-	
-	// ---------------------------
-	public String getEventName(){
-		return eventName;
-	}
-	
-	public String getDate(){
-		return date;
-	}
-	
-	public String getTime(){
-		return time;
-	}
-	
-	// ----------------------
-	
-	public boolean addThisEvent(){
 		
-		if (createFile(date)){
-			if(writeFile(date,eventName)){
-				return true;
-			}
-			else 
-				return false;
-		}
-		else
-			return true;
-	}
-	
-	// -------------------------------
-	public boolean createFile(String date) {
-		boolean flag_createFile = true;
-		File newFile = new File(date + ".txt");
-		try {
-			if (newFile.createNewFile()) {
-				System.out.println(date + " is ready for use");
-			} else {
-				System.err.println(date + " already exists!");
-				flag_createFile = false;
-			}
+		// ------------testing----------------------------
+		public static void main(String[] args) {
+			ArrayList<String> input1 = new ArrayList<String>();
+			input1.add("eat maggie mee");
+			input1.add("tmr");
+			input1.add("12pm");
 			
-		} catch (IOException ioEX) {
-			System.err.println(date + " cannot be created for some reason!");
+			TNotesStorage test = new TNotesStorage(input1);
+			if(test.addNewFile()){
+				System.out.println("added new event");
+			}
+			ArrayList<String> input2 = new ArrayList<String>();
+			input2.add("eat rice");
+			input2.add("tmr");
+			input2.add("12pm");
+			TNotesStorage test2 = new TNotesStorage(input2);
+			if(test.addToExistingFile()){
+				System.out.println("added new event");
+			}
 		}
-		return flag_createFile;
-	}
-	
-	public boolean writeFile(String date, String eventName) {
-		boolean flag_writeFile = true;
-		try {
-			FileWriter fw = new FileWriter(date, true);
-			PrintWriter pw = new PrintWriter(fw);
-
-			pw.println(eventName+" at "+time);
-			pw.close();
-
-		} catch (IOException e) {
-			System.out.println("No such file!");
-			flag_writeFile = false;
+		
+		
+		// ----------------------
+		
+		public boolean addNewFile(){
+			
+			if (createFile(taskFile.getTaskFileName())){
+				if(writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())){
+					return true;
+				}
+				else 
+					return false;
+			}
+			else
+				return true;
 		}
-		return flag_writeFile;
-	}
+		
+		public boolean addToExistingFile() {
+				boolean flag_addToExistingFile = true;
+				if(writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())){
+					flag_addToExistingFile = true;
+				}
+				
+				else{
+					flag_addToExistingFile = false;
+					System.out.println("addToExistingFile has an error");
+				}
+				return flag_addToExistingFile;
+		}
+		
+		
+		// ------------------------------------------------------------------------------
+		public boolean createFile(String fileName) {
+			boolean flag_createFile = true;
+			File newFile = new File(fileName);
+			try {
+				if (newFile.createNewFile()) {
+					System.out.println(fileName + " is ready for use");
+				} else {
+					System.err.println(fileName + " already exists!");
+					flag_createFile = false;
+				}
+				
+			} catch (IOException ioEX) {
+				System.err.println(fileName + " cannot be created for some reason!");
+			}
+			return flag_createFile;
+		}
+		
+		public boolean writeFile(String fileName, String event, String time) {
+			boolean flag_writeFile = true;
+			try {
+				FileWriter fw = new FileWriter(fileName, true);
+				PrintWriter pw = new PrintWriter(fw);
+
+				pw.println(event +" at "+ time);
+				pw.close();
+
+			} catch (IOException e) {
+				System.out.println("No such file!");
+				flag_writeFile = false;
+			}
+			return flag_writeFile;
+		}
+
 
 }
---
+++ src/taskFile.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class taskFile {
+	private static String event;
+	private static String date; 
+	private static String time;
+	private static String taskFileName;
+	
+	// Constructor
+	public taskFile (ArrayList<String> list){
+		event=list.get(0);
+		date=list.get(1);
+		time=list.get(2);
+		taskFileName = date +".txt";
+	}
+	
+	// Getters
+	public static String getEvent(){
+		return event;
+	}
+	
+	public static String getTime(){
+		return time;
+	}
+
+	public static String getDate() {	
+		return date;
+	}
+
+	// Mutator
+	public static String getTaskFileName(){
+		return taskFileName;
+	}
+	
+	
+	
+	
+	
+	
+
+}
