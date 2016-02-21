d873d1be8868ffd5b7b61225846c8eda26efdd56
Joelle Lim <joellelim94@gmail.com>
2016-02-21 11:34:48 +0800

    updated methods

--- src/TNotesLogic.java
+++ src/TNotesLogic.java
@@ -7,15 +7,35 @@ public class TNotesLogic {
 				
 	}
 	
-	public adamObject addEvent(String secondPart){
+	public adamObject createEvent(String secondPart){
 		TNotesParser parser = new TNotesParser();
 		ArrayList<String> list = new ArrayList<String>();		
 		list = parser.checkCommand(// inputhere);
 		// pass to adam, create file, return false/true, get error details	
 				
-				
+			
 		return adamObject;		
 	}
+	
+	public boolean deleteEvent(String eventName){
+	// pass to adam to delete? or i delete ?
+		
+	return flag;	
+	}
+	
+	public boolean redo(){
+		
+	}
+	
+	public boolean undo(){
+		
+	}
+	
+	public boolean editEvent(){
+		
+	}
+	
+	
 	
 	
 }
