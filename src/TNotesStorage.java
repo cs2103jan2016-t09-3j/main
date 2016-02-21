641d72097384562da6b4ad201a58ff2a40b86505
Joelle Lim <joellelim94@gmail.com>
2016-02-21 17:14:54 +0800

    no message

--- src/TNotesStorage.java
+++ src/TNotesStorage.java
@@ -3,87 +3,82 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+
 // Note that must change storage to NotesStorage when using commit
 public class TNotesStorage {
-	
-private ArrayList<taskFile> arrayDate = new ArrayList<taskFile>();
-	
-		
-		public TNotesStorage () {
-			
-		}
-		
-		// ------------testing----------------------------
-		public static void main(String[] args) {
-			
-		}
-		
-		
-		// ----------------------
-		
-		public boolean addNewFile(taskFile newTaskFile){
-			arrayDate.add(newTaskFile);
-			
-			if (createFile(taskFile.getTaskFileName())){
-				if(writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())){
-					return true;
-				}
-				else 
-					return false;
-			}
-			else
+
+	private ArrayList<taskFile> arrayDate = new ArrayList<taskFile>();
+
+	public TNotesStorage() {
+
+	}
+
+	// ------------testing----------------------------
+	public static void main(String[] args) {
+
+	}
+
+	// ----------------------
+
+	public boolean addNewFile(taskFile newTaskFile) {
+		arrayDate.add(newTaskFile);
+
+		if (createFile(taskFile.getTaskFileName())) {
+			if (writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())) {
 				return true;
+			} else
+				return false;
+		} else
+			return true;
+	}
+
+	public boolean addToExistingFile(taskFile newTaskFile) {
+		arrayDate.add(newTaskFile);
+
+		boolean flag_addToExistingFile = true;
+		if (writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())) {
+			flag_addToExistingFile = true;
 		}
-		
-		public boolean addToExistingFile(taskFile newTaskFile) {
-			arrayDate.add(newTaskFile);
-			
-				boolean flag_addToExistingFile = true;
-				if(writeFile(taskFile.getTaskFileName(), taskFile.getEvent(), taskFile.getTime())){
-					flag_addToExistingFile = true;
-				}
-				
-				else{
-					flag_addToExistingFile = false;
-					System.out.println("addToExistingFile has an error");
-				}
-				return flag_addToExistingFile;
+
+		else {
+			flag_addToExistingFile = false;
+			System.out.println("addToExistingFile has an error");
 		}
-		
-		
-		// ------------------------------------------------------------------------------
-		public boolean createFile(String fileName) {
-			boolean flag_createFile = true;
-			File newFile = new File(fileName);
-			try {
-				if (newFile.createNewFile()) {
-					System.out.println(fileName + " is ready for use");
-				} else {
-					System.err.println(fileName + " already exists!");
-					flag_createFile = false;
-				}
-				
-			} catch (IOException ioEX) {
-				System.err.println(fileName + " cannot be created for some reason!");
+		return flag_addToExistingFile;
+	}
+
+	// ------------------------------------------------------------------------------
+	public boolean createFile(String fileName) {
+		boolean flag_createFile = true;
+		File newFile = new File(fileName);
+		try {
+			if (newFile.createNewFile()) {
+				System.out.println(fileName + " is ready for use");
+			} else {
+				System.err.println(fileName + " already exists!");
+				flag_createFile = false;
 			}
-			return flag_createFile;
+
+		} catch (IOException ioEX) {
+			System.err.println(fileName + " cannot be created for some reason!");
 		}
-		
-		public boolean writeFile(String fileName, String event, String time) {
-			boolean flag_writeFile = true;
-			try {
-				FileWriter fw = new FileWriter(fileName, true);
-				PrintWriter pw = new PrintWriter(fw);
+		return flag_createFile;
+	}
 
-				pw.println(event +" at "+ time);
-				pw.close();
+	public boolean writeFile(String fileName, String event, String time) {
+		boolean flag_writeFile = true;
+		try {
+			FileWriter fw = new FileWriter(fileName, true);
+			PrintWriter pw = new PrintWriter(fw);
 
-			} catch (IOException e) {
-				System.out.println("No such file!");
-				flag_writeFile = false;
-			}
-			return flag_writeFile;
+			pw.println(event + " at " + time);
+			pw.close();
+
+		} catch (IOException e) {
+			System.out.println("No such file!");
+			flag_writeFile = false;
 		}
-
+		return flag_writeFile;
+	}
 
 }
