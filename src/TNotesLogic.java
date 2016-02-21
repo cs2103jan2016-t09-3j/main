7937b5de682ec3d4b2cd9836ec0246a02f3e899a
Joelle Lim <joellelim94@gmail.com>
2016-02-21 10:55:01 +0800

    renamed my file

---
+++ src/TNotesLogic.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class TNotesLogic {
+
+	public static void main(String[] args) {
+		
+				
+	}
+	
+	public adamObject addEvent(String secondPart){
+		TNotesParser parser = new TNotesParser();
+		ArrayList<String> list = new ArrayList<String>();		
+		list = parser.checkCommand(// inputhere);
+		// pass to adam, create file, return false/true, get error details	
+				
+				
+		return adamObject;		
+	}
+	
+	
+}
