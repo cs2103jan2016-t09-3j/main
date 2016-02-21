497e732804342ce5457247871feadb56953fdba8
Adam Chin <adamcjh1@gmail.com>
2016-02-21 14:38:57 +0800

    help

--- src/TNotesLogic.java
+++ src/TNotesLogic.java
@@ -2,22 +2,30 @@ import java.util.ArrayList;
 
 public class TNotesLogic {
 
-	
 	TNotesParser parser = new TNotesParser();
-	
+
 	ArrayList<String> list = new ArrayList<String>();
-	
-	
-	public boolean addTask(String whatever){
+
+	public boolean addTask(String whatever) {
 		list = parser.checkCommand(whatever);
 		TNotesStorage storage = new TNotesStorage(list);
-		if(storage.addThisEvent()){
+		if (storage.addThisEvent()) {
 			return true;
-		}
-		else{
+		} else {
 			return false;
 		}
 	}
-	
-	
+
+	public boolean deleteTask(String whatever) {
+		return false;
+	}
+
+	public boolean readTask() {
+		return false;
+	}
+
+	public boolean editTask(String whatever) {
+		return false;
+	}
+
 }
