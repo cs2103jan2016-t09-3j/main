c4f137cbf96b15e48e9ea6ed1effce54d82df80f
Joelle Lim <joellelim94@gmail.com>
2016-02-23 19:33:38 +0800

    Added Folder class : Can create / delete folders. Folders are stored in the same directory as eclipse workspace

---
+++ src/Folder.java
@@ -0,0 +1,54 @@
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Folder {
+	
+	public Folder() {
+	
+		
+	}	
+
+	public static void main(String[] args) {
+
+		String newFolderName = "TNote"; // type in name of folder
+		createNewFolder(newFolderName);
+
+		char xdeleteDir = 'y';
+		if (xdeleteDir == 'y') {
+			deleteExistingFolder(newFolderName);
+		}
+
+	}
+
+	static void createNewFolder(String newFolderName) {
+
+		File folder = new File(newFolderName);
+
+		try {
+			if (folder.exists()) {
+				System.out.println(folder.getName() + " Folder already exists!");
+			} else {
+				folder.mkdir();
+				System.out.println(folder.getName() + " Folder Created!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static void deleteExistingFolder(String folderName) {
+		
+		Path xPath = Paths.get(folderName);
+		
+		try {
+			Files.delete(xPath);
+			System.out.println("Deleted " + folderName + " Folder!");
+		} catch (Exception e) {
+			System.out.println("Could not delete Folder!");
+		}
+	}
+
+}
