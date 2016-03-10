173759db14dbf8fc5ccfb3c798b51332a695dbca
Joelle Lim <joellelim94@gmail.com>
2016-03-10 17:46:23 +0800

    Changed the primary stage title to T-Note

--- src/UI/MainApp.java
+++ src/UI/MainApp.java
@@ -21,7 +21,7 @@ public class MainApp extends Application {
 	    @Override
 	    public void start(Stage primaryStage) {
 	        this.primaryStage = primaryStage;
-	        this.primaryStage.setTitle("AddressApp");
+	        this.primaryStage.setTitle("T-Note");
 
 	        initRootLayout();
 
