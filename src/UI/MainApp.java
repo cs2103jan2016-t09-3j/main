79284ad55b66d53c7e3e69e1e14d15986e4985dc
Joelle Lim <joellelim94@gmail.com>
2016-03-10 12:12:32 +0800

    Added empty GUI

---
+++ src/UI/MainApp.java
@@ -0,0 +1,79 @@
+package UI;
+
+
+import  java.io.IOException;
+
+//import ch.makery.address.model.Person;
+import javafx.application.Application;
+//import javafx.collections.*;
+import javafx.collections.FXCollections;
+import javafx.fxml.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	 
+		public Stage primaryStage;
+	    public BorderPane rootLayout;
+
+	    @Override
+	    public void start(Stage primaryStage) {
+	        this.primaryStage = primaryStage;
+	        this.primaryStage.setTitle("AddressApp");
+
+	        initRootLayout();
+
+	        showPersonOverview();
+	    }
+
+	    /**
+	     * Initializes the root layout.
+	     */
+	    public void initRootLayout() {
+	        try {
+	            // Load root layout from fxml file.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(MainApp.class.getResource("RootLayout.fxml"));
+	            rootLayout = (BorderPane) loader.load();
+
+	            // Show the scene containing the root layout.
+	            Scene scene = new Scene(rootLayout);
+	            primaryStage.setScene(scene);
+	            primaryStage.show();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    /**
+	     * Shows the person overview inside the root layout.
+	     */
+	    public void showPersonOverview() {
+	        try {
+	            // Load TNotesOverview overview.
+	            FXMLLoader loader = new FXMLLoader();
+	            loader.setLocation(MainApp.class.getResource("TNotesOverview.fxml"));
+	            AnchorPane TNotesOverview = (AnchorPane) loader.load();
+
+	            // Set person overview into the center of root layout.
+	            rootLayout.setCenter(TNotesOverview);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    /**
+	     * Returns the main stage.
+	     * @return
+	     */
+	    public Stage getPrimaryStage() {
+	        return primaryStage;
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+
+}
