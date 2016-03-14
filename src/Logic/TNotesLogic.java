0433d1abf168b4b5577896c4459f828f17b9096b
Adam Chin <adamcjh1@gmail.com>
2016-03-14 17:16:49 +0800

    slight changes

--- src/Logic/TNotesLogic.java
+++ src/Logic/TNotesLogic.java
@@ -2,6 +2,7 @@ package Logic;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import static org.junit.Assert.assertTrue;
@@ -18,188 +19,235 @@ public class TNotesLogic {
 	// for blocking out timings, need to check if the timing for task to be
 	// added is available.
 	// got deadline task, means do by 4pm
-	// adam u gonna remove fromParser[0] in the end cos of ur extra classes. so i code for that.
-	public boolean addTask(ArrayList<String> fromParser) {
-		try{
-		
-		ArrayList<String> stringList = storage.readFromMasterFile();
-		TaskFile currentFile = new TaskFile();
-		boolean isRecurr = false;
-		String importance = new String();
-		
-		//assert size != 0
-		currentFile.setName(fromParser.remove(0));
-		
-		if(fromParser.contains("important")) {
-			importance = fromParser.remove(fromParser.indexOf("importance"));
-			currentFile.setImportance(importance);
-		}
-		
-		
-		if(fromParser.contains("every")) {
-			int indexOfRecurKeyWord = fromParser.indexOf("every");
-			String recurArgument = fromParser.remove(indexOfRecurKeyWord+1);
-			fromParser.remove("every");
-			currentFile.setIsRecurr(true);
-		}
-		
-		
-		for(String details: fromParser) {
-			if(!details.contains(":") && !details.contains("-")) {
-				currentFile.setDetails(details);
-				fromParser.remove(details);
+	// adam u gonna remove fromParser[0] in the end cos of ur extra classes. so
+	// i code for that.
+	public TaskFile addTask(ArrayList<String> fromParser) {
+		try {
+
+			ArrayList<String> stringList = storage.readFromMasterFile();
+			TaskFile currentFile = new TaskFile();
+			boolean isRecurr = false;
+			String importance = new String();
+			String recurArgument = new String();
+			// assert size != 0
+			currentFile.setName(fromParser.remove(0));
+
+			if (fromParser.contains("important")) {
+				importance = fromParser.remove(fromParser.indexOf("importance"));
+				currentFile.setImportance(importance);
 			}
-		}
-		System.err.println(fromParser.toString());
-		switch (fromParser.size()) {
-		case 1:
 
-//		// case 2:	
-//		// Floating task : add call mum 
-//		case 2:
-//			currentFile.setTask(fromParser.get(1));
-//			fromParser.add("floating");
-//			break;
-//		// case 3:	
-//		// add call mum important / add call mum due 11-3-2016 / add call mum at 3:00	
-//		// add call mum details tell her to buy apples	
-//		case 3:
-//			currentFile.setTask(fromParser.get(1));
-//			if(fromParser.get(2).equals("important")){
-//				currentFile.setImportance(fromParser.get(2));
-//				fromParser.add("size2 important");
-
-			
-			if (fromParser.get(0).contains("-")) {
-				currentFile.setStartDate(fromParser.get(0));
-			} else {
-				assertTrue(fromParser.get(0).contains(":"));
-				currentFile.setStartTime(fromParser.get(0));
-
+			if (fromParser.contains("every")) {
+				int indexOfRecurKeyWord = fromParser.indexOf("every");
+				recurArgument = fromParser.remove(indexOfRecurKeyWord + 1);
+				fromParser.remove("every");
+				currentFile.setIsRecurr(true);
 			}
-			break;
-		case 2:
-			if (fromParser.get(0).contains("-")) {
-				currentFile.setStartDate(fromParser.get(2));
-				
-				if (fromParser.get(1).contains("-")) {
-					currentFile.setEndDate(fromParser.get(1));
-				} else { 
-					assertTrue(fromParser.get(1).contains(":"));
-					currentFile.setStartTime(fromParser.get(1));
-				} 
-				
-			} else if (fromParser.get(0).contains(":")) {
-				currentFile.setStartTime(fromParser.get(0));
-				
-				if (fromParser.get(1).contains("-")) {
-					currentFile.setEndDate(fromParser.get(1));
-				} else { 
-					assertTrue(fromParser.get(1).contains(":"));
-					currentFile.setStartTime(fromParser.get(1));
-				} 
 
+			for (String details : fromParser) {
+				if (!details.contains(":") && !details.contains("-")) {
+					currentFile.setDetails(details);
+					fromParser.remove(details);
+				}
 			}
-			break;
-		case 3:
-			if (fromParser.get(0).contains("-")) {
-				currentFile.setStartDate(fromParser.get(0));
-				
-				if (fromParser.get(1).contains(":")) {
-					currentFile.setStartTime(fromParser.get(1));
-					
-					if (fromParser.get(2).contains("-")) {
-						currentFile.setEndDate(fromParser.get(2));
-					} else { 
+			System.err.println(fromParser.toString());
+			switch (fromParser.size()) {
+			case 1:
+
+				// // case 2:
+				// // Floating task : add call mum
+				// case 2:
+				// currentFile.setTask(fromParser.get(1));
+				// fromParser.add("floating");
+				// break;
+				// // case 3:
+				// // add call mum important / add call mum due 11-3-2016 / add
+				// call mum at 3:00
+				// // add call mum details tell her to buy apples
+				// case 3:
+				// currentFile.setTask(fromParser.get(1));
+				// if(fromParser.get(2).equals("important")){
+				// currentFile.setImportance(fromParser.get(2));
+				// fromParser.add("size2 important");
+
+				if (fromParser.get(0).contains("-")) {
+					currentFile.setStartDate(fromParser.get(0));
+				} else {
+					assertTrue(fromParser.get(0).contains(":"));
+					currentFile.setStartTime(fromParser.get(0));
+
+				}
+				break;
+			case 2:
+				if (fromParser.get(0).contains("-")) {
+					currentFile.setStartDate(fromParser.get(2));
+
+					if (fromParser.get(1).contains("-")) {
+						currentFile.setEndDate(fromParser.get(1));
+					} else {
+						assertTrue(fromParser.get(1).contains(":"));
+						currentFile.setStartTime(fromParser.get(1));
+					}
+
+				} else if (fromParser.get(0).contains(":")) {
+					currentFile.setStartTime(fromParser.get(0));
+
+					if (fromParser.get(1).contains("-")) {
+						currentFile.setEndDate(fromParser.get(1));
+					} else {
+						assertTrue(fromParser.get(1).contains(":"));
+						currentFile.setStartTime(fromParser.get(1));
+					}
+
+				}
+				break;
+			case 3:
+				if (fromParser.get(0).contains("-")) {
+					currentFile.setStartDate(fromParser.get(0));
+
+					if (fromParser.get(1).contains(":")) {
+						currentFile.setStartTime(fromParser.get(1));
+
+						if (fromParser.get(2).contains("-")) {
+							currentFile.setEndDate(fromParser.get(2));
+						} else {
+							assertTrue(fromParser.get(2).contains(":"));
+							currentFile.setStartTime(fromParser.get(2));
+						}
+
+					} else if (fromParser.get(1).contains("-")) {
+						currentFile.setEndDate(fromParser.get(1));
+
 						assertTrue(fromParser.get(2).contains(":"));
-						currentFile.setStartTime(fromParser.get(2));
-					} 
-					
-				} else if (fromParser.get(1).contains("-")) {
+						currentFile.setEndTime(fromParser.get(2));
+					}
+
+				} else if (fromParser.get(0).contains(":")) {
+					currentFile.setStartTime(fromParser.get(0));
+
+					assertTrue(fromParser.get(1).contains("-"));
 					currentFile.setEndDate(fromParser.get(1));
-					
+
 					assertTrue(fromParser.get(2).contains(":"));
 					currentFile.setEndTime(fromParser.get(2));
 				}
-				
-			} else if (fromParser.get(0).contains(":")) {
-				currentFile.setStartTime(fromParser.get(0));
-				
-				assertTrue(fromParser.get(1).contains("-"));
-				currentFile.setEndDate(fromParser.get(1));
-					
-				assertTrue(fromParser.get(2).contains(":"));
-				currentFile.setEndTime(fromParser.get(2));
-			}
-			break;			
-			
-		case 4:
+				break;
 
-			
-			assertTrue(fromParser.get(0).contains("-"));
-			currentFile.setStartDate(fromParser.get(0));
-			
-			assertTrue(fromParser.get(1).contains(":"));
-			currentFile.setStartTime(fromParser.get(1));
-				
-			assertTrue(fromParser.get(2).contains("-"));
-			currentFile.setEndDate(fromParser.get(2));
-			
-			assertTrue(fromParser.get(3).contains(":"));
-			currentFile.setEndTime(fromParser.get(3));
-		
-		break;
-		
-		default:
-			assertEquals(0, fromParser.size());
-		}
-		
-		currentFile.setUpTaskFile();
-		return storage.addTask(currentFile);
-	
-		// TaskFile currentFile = new TaskFile(fromParser.get(1));
-		// currentFile.setTask(fromParser.get(1));
-		// String punctuation = ":"
-		// if (fromUI.getTime.contain(punctaions)
-		// for (TaskFile newTask : taskList) {
-		// // check for same date
-		// if (currentFile.getStartDate().equals(newTask.getStartDate())) {
-		// // check if same start time, else check end time, then return
-		// // error.
-		// if (currentFile.getStartTime().equals(newTask.getStartTime())) {
-		// return false;
-		// } else if (currentFile.getIsMeeting()) {// throw to top of if loop
-		//
-		// //Adam this wont work btw 13:00 cannot be parsed into an integer.
-		// Lets just swap to date objects?
-		// // or i create a compare date/time method.
-		// if (Integer.parseInt(currentFile.getStartTime()) <
-		// Integer.parseInt(newTask.getStartTime())) {
-		// if (Integer.parseInt(currentFile.getEndTime()) <=
-		// Integer.parseInt(newTask.getStartTime())) {
-		// break;
-		// } else {
-		// return false;
-		// }
-		//
-		// } else if (Integer.parseInt(currentFile.getStartTime()) > Integer
-		// .parseInt(newTask.getStartTime())) {
-		// if (Integer.parseInt(currentFile.getStartTime()) >=
-		// Integer.parseInt(newTask.getEndTime())) {
-		// break;
-		// } else {
-		// return false;
-		// }
-		// }
-		// }
-		// }
-		// }
-		
-		} catch (AssertionError aE) {
-			//means the switch statement got invalid arguments
-			//throw instead of return
-			return false;
-		}
+			case 4:
+
+				assertTrue(fromParser.get(0).contains("-"));
+				currentFile.setStartDate(fromParser.get(0));
+
+				assertTrue(fromParser.get(1).contains(":"));
+				currentFile.setStartTime(fromParser.get(1));
+
+				assertTrue(fromParser.get(2).contains("-"));
+				currentFile.setEndDate(fromParser.get(2));
+
+				assertTrue(fromParser.get(3).contains(":"));
+				currentFile.setEndTime(fromParser.get(3));
+
+				break;
+
+			default:
+				assertEquals(0, fromParser.size());
+			}
+			currentFile.setUpTaskFile();
+			if (currentFile.getIsRecurring()) {
+				Calendar firstRecurDate = currentFile.getStartCal();
+				Calendar endRecurDate = currentFile.getEndCal();
+				if (recurArgument.equals("day")) {
+					for (int i = 0; i < 10; i++) {
+						TaskFile test = 
+						firstRecurDate.add(Calendar.DATE, 1);
+						currentFile.setStartCal();
+						endRecurDate.add(Calendar.DATE, 1);
+						currentFile.setEndCal();
+						storage.addRecurTask(currentFile);
+					}
+				} else if (recurArgument.equals("week")) {
+					for (int i = 0; i < 3; i++) {
+						firstRecurDate.add(Calendar.DATE, 7);
+						currentFile.setStartCal();
+						endRecurDate.add(Calendar.DATE, 7);
+						currentFile.setEndCal();
+					}
+					storage.addRecurTask(currentFile);
+				}else if (recurArgument.equals("month")){
+					for(int i = 0; i<4;i++){
+						firstRecurDate.add(Calendar.MONTH,1);
+						currentFile.setStartCal(firstRecurDate);
+						endRecurDate.add(Calendar.MONTH,1 );
+						currentFile.setEndCal();
+					}
+					storage.addRecurTask(currentFile);
+				}
+				else if(recurArgument.equals("year")){
+					for(int i = 0; i < 1;i++){
+						firstRecurDate.add(Calendar.YEAR, 1);
+						currentFile.setStartCal();
+						endRecurDate.add(Calendar.YEAR, 1);
+					}
+					storage.addRecurTask(currentFile);					
+				}
+				else{
+					System.out.println("Error");
+				}
+			}
+			if(storage.addTask(currentFile)){
+				return currentFile;
+			}
+
+
+			// TaskFile currentFile = new TaskFile(fromParser.get(1));
+			// currentFile.setTask(fromParser.get(1));
+			// String punctuation = ":"
+			// if (fromUI.getTime.contain(punctaions)
+			// for (TaskFile newTask : taskList) {
+			// // check for same date
+			// if (currentFile.getStartDate().equals(newTask.getStartDate())) {
+			// // check if same start time, else check end time, then return
+			// // error.
+			// if (currentFile.getStartTime().equals(newTask.getStartTime())) {
+			// return false;
+			// } else if (currentFile.getIsMeeting()) {// throw to top of if
+			// loop
+			//
+			// //Adam this wont work btw 13:00 cannot be parsed into an integer.
+			// Lets just swap to date objects?
+			// // or i create a compare date/time method.
+			// if (Integer.parseInt(currentFile.getStartTime()) <
+			// Integer.parseInt(newTask.getStartTime())) {
+			// if (Integer.parseInt(currentFile.getEndTime()) <=
+			// Integer.parseInt(newTask.getStartTime())) {
+			// break;
+			// } else {
+			// return false;
+			// }
+			//
+			// } else if (Integer.parseInt(currentFile.getStartTime()) > Integer
+			// .parseInt(newTask.getStartTime())) {
+			// if (Integer.parseInt(currentFile.getStartTime()) >=
+			// Integer.parseInt(newTask.getEndTime())) {
+			// break;
+			// } else {
+			// return false;
+			// }
+			// }
+			// }
+			// }
+			// }
+
+		}catch(
+
+	AssertionError aE)
+
+	{
+		// means the switch statement got invalid arguments
+		// throw instead of return
+		return false;
+	}
+
 	}
 
 	// currently has issue, need to make a new object, compare it, then remove
@@ -374,12 +422,11 @@ public class TNotesLogic {
 	// return date;
 	// }
 
-
 	public static void main(String[] args) {
 		TNotesLogic tNote = new TNotesLogic();
 		ArrayList<String> list = new ArrayList<String>();
 
-		//list.add("add");
+		// list.add("add");
 		list.add("call lalala");
 		list.add("2016-3-19");
 		list.add("1:00");
@@ -392,4 +439,4 @@ public class TNotesLogic {
 		}
 	}
 
-}
+}}
