c1f4f94e18997c3a2920473a41b41e173ede8c2a
Joelle Lim <joellelim94@gmail.com>
2016-03-23 23:49:07 +0800

    [U] not very useful JunitTest

--- src/UI/TNotesUITest.java
+++ src/UI/TNotesUITest.java
@@ -1,19 +1,29 @@
 package UI;
 
 import static org.junit.Assert.*;
-
+import java.util.*;
 
 import org.junit.Test;
 
 public class TNotesUITest {
 	
+	TNotesUI tnoteUI = new TNotesUI();
+	ArrayList<String> parserOutput = new ArrayList<String>();
+	
 	@Test
-	public void testGetWelcomeMessage() {
-		TNotesUI tnoteUI = new TNotesUI();
+	public void testGetWelcomeMessage() {	
 		String output = tnoteUI.getWelcomeMessage();
 		assertEquals("Hello, welcome to T-Note. How many I help you?",output);
 	}
 	
+// need to refactor my methods to actually do this	
+//	@Test
+//	public void testAddExecuteCommand(){
+//		parserOutput.add("add");
+//		parserOutput.add("call mum");
+//		String output = tnoteUI.executeCommand();
+//	}
+//	
 	
 
 }
